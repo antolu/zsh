2126f3e6779fffbeda73dbb85d70df94b0836cab
Anton Lu <anton@haochen.lu>
2019-07-06 18:33:45 +0200

    shebang, chmod +x, it works!

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 rm ~/.zshrc
 
@@ -6,3 +8,5 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+curl https://raw.githubusercontent.com/antolu/zsh/master/.zpreztorc > ~/.zpreztorc
+chsh -s /bin/zsh
